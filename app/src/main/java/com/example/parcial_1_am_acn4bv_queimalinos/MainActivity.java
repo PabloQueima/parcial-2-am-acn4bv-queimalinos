46cd782839b41e7f53b3204356311968d887247b
Pablo Queimaliños <pablo.queimalinos@davinci.edu.ar>
2025-10-04 18:35:04 -0300

    feat: agregar layout inicial

--- app/src/main/java/com/example/parcial_1_am_acn4bv_queimalinos/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_queimalinos/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.parcial_1_am_acn4bv_queimalinos;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Evento del botón
+        Button botonPrueba = findViewById(R.id.botonPrueba);
+        botonPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "¡Botón presionado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Elemento dinámico creado desde Java
+        Button botonDinamico = new Button(this);
+        botonDinamico.setText("Botón dinámico");
+        ((LinearLayout) findViewById(R.id.contenedorPrincipal)).addView(botonDinamico);
+
+        botonDinamico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "¡Botón dinámico presionado!", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
